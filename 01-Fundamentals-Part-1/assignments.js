0e650387289bc7ad6ee966a36c0d7e4bb685e6f3
georgina puig <georginaxcodes@gmail.com>
2021-01-07 03:58:38 -0600

    LECTURE: Values and Variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,16 @@
+// LECTURE: Values and Variables
+
+// 1.Declare variables called 'country', 'continent'and 'population' and assign their values according to your own country(population in millions)
+let country = 'Uruguay';
+let continent = 'South America';
+let population = 3400000;
+
+// 2.Log their values to the console
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// LECTURE: Data Types
+
+// 1.Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Booleanvalue. Also declare a variable'language', but don't assign it any value yet
+// 2.Log the types of 'isIsland', 'population', 'country'and 'language'to the console
