74d28fcd15dac3e2bbfb746479bb8ecd6b8e3e1e
georgina puig <georginaxcodes@gmail.com>
2021-01-23 12:33:02 -0300

    LECTURE: Taking Decisions: if / else Statements (1)

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -58,6 +58,23 @@ console.log(description1);
 // LECTURE: Strings and Template Literals
 
 // 1.Recreate the 'description' variable from the last assignment, this time using the template literal syntax
-const description2 = `${country} is in ${continent}, and its ${population} ' million people speak ' ${language}`;
+const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
 console.log(description2);
+
+// LECTURE: Taking Decisions: if / else Statements
+
+// 1.If your country's population is greater that 33 million, log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
+if (country > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(`${country}'s population is ${33 - population} million below average`);
+}
+
+// 2.After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original
+
+// LECTURE: Type Conversion and Coercion
+
+// 1.Predict the result of these 5 operations without executing them:'9' -'5';'19' -'13' + '17';'19' -'13' + 17;'123' < 57;5 + 6 + '4' + 9 -4 -2;
+
+// 2.Execute the operations to check if you were right
