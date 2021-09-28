02848d0c3b3dc1efb603f49b1756fe2c15e7f552
georgina <georginaxpuig@gmail.com>
2021-09-28 01:09:58 -0300

    order files

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,73 @@
+////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+
+// const johnMass = 78;
+// const johnHeight = 1.69;
+
+// const markMass = 92;
+// const markHeight = 1.95;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnBMI = johnMass / johnHeight ** 2;
+const markBMI = markMass / markHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI  ;
+
+console.log(johnBMI, markBMI, markHigherBMI);
+
+////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`);
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's! (${markBMI})"`);
+}
+
+////////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,3 +1,4 @@
+////////////////////////////////////
 // Coding Challenge #1
 
 /*
@@ -21,9 +22,9 @@ function calcAverage(score1, score2, score3) {
 
 function checkWinner(avgDolphins, avgKoalas) {
   if (avgDolphins >= (avgKoalas * 2)) { 
-    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`); 
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`); 
   } else if (avgKoalas >= (avgDolphins * 2)) { 
-    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`); 
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`); 
   } else {
     console.log(`No team wins. Koalas: ${avgKoalas} vs. Dolphins: ${avgDolphins}`);
   }
@@ -31,3 +32,19 @@ function checkWinner(avgDolphins, avgKoalas) {
 
 checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49)); // TEST DATA 1
 checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27)); // TEST DATA 2
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+*/
+
+const bills = [125, 555, 44];
