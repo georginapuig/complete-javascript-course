0583bcf3b96c2b1ba40f98a30a8511030c33ce83
georgina <georginaxpuig@gmail.com>
2021-10-02 00:01:12 -0300

    02 - Coding Challenge #4

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -128,3 +128,53 @@ if (john.bmi > mark.bmi) {
 } else if (mark.bmi > john.bmi) {
   console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`)
 }
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+const bills2 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]; // 1
+const tips2 = []; // 2
+const totals2 = []; // 2
+
+// 3
+const calcTip2 = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+for (let i = 0; i < bills2.length; i++) {
+  tips2.push(calcTip2(bills2[i]));
+  totals2.push(tips2[i] + bills2[i]);
+}
+
+console.log(bills2, tips2, totals2);
+
+//  BONUS
+function calcAverage(arr) {
+  // 4.1
+  let sum = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length; // 4.2
+}
+
+console.log(calcAverage(totals2)); // 4.2
+console.log(calcAverage([2, 3, 7]));
