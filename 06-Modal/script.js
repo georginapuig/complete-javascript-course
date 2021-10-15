15b886f0ff080d60ca4cd0f9f26d125a300e54dc
georgina <georginaxpuig@gmail.com>
2021-10-15 00:19:48 -0300

    06 - refactor show, hide modal

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -12,12 +12,10 @@ for (let i = 0; i < btnsShowModal.length; i++) {
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
