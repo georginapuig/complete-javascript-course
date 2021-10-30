7ae3cbfadf7fe2310729d90942fa38422e3e0963
georgina <georginaxpuig@gmail.com>
2021-10-30 18:14:59 -0300

    Handling an "Esc" Keypress Event

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -20,3 +20,13 @@ for (let i = 0; i < btnsShowModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
