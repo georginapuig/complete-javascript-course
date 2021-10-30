9c3118489dacf1875605567f7df246f3067dbca7
georgina <georginaxpuig@gmail.com>
2021-10-30 18:15:34 -0300

    refactor Handling an "Esc" Keypress Event

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -24,9 +24,7 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   console.log(e.key);
 
-  if (e.key === 'Escape') {
-    if (!modal.classList.contains('hidden')) {
-      closeModal();
-    }
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
 });
