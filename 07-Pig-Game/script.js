93eaf85de279b07f3cd1c61730f18a4bdfccab5e
georgina <georginaxpuig@gmail.com>
2021-12-23 12:22:27 -0300

    pig game add starting conditions

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,6 +3,10 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+// buttons
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // starting conditions
 score0El.textContent = 0;
