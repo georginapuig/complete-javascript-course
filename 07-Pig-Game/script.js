39f2053b13b58bbf4f83fc78d5d4851f4512b184
georgina <georginaxpuig@gmail.com>
2021-12-31 11:49:29 -0300

    generating a random dice roll and display dice

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -12,3 +12,14 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function() {
+  // 1. generating a random dice roll
+  let diceNum = Math.trunc((Math.random() * 6) + 1);
+  console.log(diceNum);
+  // 2. display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src=`dice-${diceNum}.png`;
+  // 3. checked for rolled 1: if true, switch to next player
+});
