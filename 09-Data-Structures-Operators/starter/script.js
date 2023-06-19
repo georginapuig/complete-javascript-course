d2bbdf2ecdf87fd80be96809d1d6ded951b269da
georgina puig <georginaxpuig@gmail.com>
2023-06-19 17:54:20 -0300

    spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,27 +1,29 @@
 'use strict';
 
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// destructuring objects
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
 
 // const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
@@ -48,15 +50,61 @@
 // } = openingHours;
 // console.log(open, close);
 
-const restaurant = {
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(starterIndex, mainIndex, time, address);
-  },
+// const restaurant = {
+//   orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+//     console.log(starterIndex, mainIndex, time, address);
+//   },
+// };
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// spread operator
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr); // [ 1, 2, 7, 8, 9 ]
+
+// const goodNewArr = [...arr];
+// console.log(1, 2, goodNewArr);
+
+// const restaurant = {
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// };
+
+const menu = [...restaurant.mainMenu, 'Burguer'];
+// console.log(menu); // [ 'Pizza', 'Pasta', 'Risotto', 'Burguer' ]
+
+const name = 'August';
+const letters = [...name, 'o'];
+// console.log(letters);
+
+const newRestaurant = { ...restaurant, founder: 'Ana' };
+// console.log(newRestaurant);
+
+// spread, right side of =
+const arrSpread = [1, 2, ...[4, 5]];
+// console.log(arrSpread); // [ 1, 2, 4, 5 ]
+
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others); // 1 2 [ 3, 4, 5 ]
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+// console.log(pizza, risotto, otherFood); // Pizza Risotto [ 'Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad' ]
+
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays) // { thu: { open: 12, close: 22 }, fri: { open: 11, close: 23 } }
+
+const add = function (...numbers) {
+  console.log(numbers);
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via del Sole, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+add(1, 2); // [ 1, 2 ]
+add(3, 4, 5, 6); // [ 3, 4, 5, 6 ]
