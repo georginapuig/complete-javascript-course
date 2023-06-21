0de5affa0ed7b29d19c964082bb07b432b28885f
georgina puig <georginaxpuig@gmail.com>
2023-06-21 13:55:59 -0300

    optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -21,6 +21,26 @@ const restaurant = {
       close: 24,
     },
   },
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 // destructuring objects
@@ -161,24 +181,73 @@ rest1.numOfGuests &&= 10;
 
 // looping arrays (for of)
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 // get element
-for (const item of menu) {
-  console.log(item); // Focaccia Bruschetta Garlic Bread Caprese Salad Pizza Pasta Risotto
-}
+// for (const item of menu) {
+//   console.log(item); // Focaccia Bruschetta Garlic Bread Caprese Salad Pizza Pasta Risotto
+// }
 
 // get index
-for (const item of menu.entries()) {
-  console.log(item) // [ 0, 'Focaccia' ] [ 1, 'Bruschetta' ] [ 2, 'Garlic Bread' ] [ 3, 'Caprese Salad' ] [ 4, 'Pizza' ] [ 5, 'Pasta' ] [ 6, 'Risotto' ]
-}
+// for (const item of menu.entries()) {
+//   console.log(item) // [ 0, 'Focaccia' ] [ 1, 'Bruschetta' ] [ 2, 'Garlic Bread' ] [ 3, 'Caprese Salad' ] [ 4, 'Pizza' ] [ 5, 'Pasta' ] [ 6, 'Risotto' ]
+// }
 
 // wo destructuring
-for (const item of menu.entries()) {
-  console.log(item[0], item[1]);
-}
+// for (const item of menu.entries()) {
+//   console.log(item[0], item[1]);
+// }
 
 // with destructuring
-for (const [index, element] of menu.entries()) {
-  console.log(index, element);
-}
+// for (const [index, element] of menu.entries()) {
+//   console.log(index, element);
+// }
+
+//  computed property names
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const openingHours = {
+//   thu: {
+//     open: 12,
+//     close: 22,
+//   },
+// }
+
+// const openingHours = {
+//   [days[3]]: { //
+//     open: 12,
+//     close: 22,
+//   },
+// }
+
+// console.log(openingHours);
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist'); // [ 'Focaccia', 'Pasta' ]
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'); // Method does not exist
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+
+// with
+console.log(users[0]?.name ?? 'User array empty');
+
+// wo
+// if (users.length > 0) console.log(users[0].name);
+// else console.log('user array empty');
